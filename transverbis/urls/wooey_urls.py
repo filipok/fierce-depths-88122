f428d2d4a80166fd8799163038b575416ac4892b
Filip <filip.gadiuta@gmail.com>
2016-01-31 16:21:33 +0200

    move wooey to code url

--- transverbis/urls/wooey_urls.py
+++ transverbis/urls/wooey_urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import include, url
 
 urlpatterns += [
     #url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('wooey.urls')),
+    url(r'^code/', include('wooey.urls')),
 ]
