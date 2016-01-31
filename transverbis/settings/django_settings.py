20c82ada494032506cdc6fc352a770a7905d3855
Filip <filip.gadiuta@gmail.com>
2016-01-31 19:04:23 +0200

    add the blog package

--- transverbis/settings/django_settings.py
+++ transverbis/settings/django_settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'djtransverbisblog',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -63,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'djtransverbisblog.context_proc.base_variables',
             ],
         },
     },
@@ -119,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_REDIRECT_URL = '/'
+WEBSITE_NAME = 'Transverbis'
+WEBSITE_SLOGAN = 'Translations'
